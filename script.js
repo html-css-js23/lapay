224155fcc3bbc448bd0aad77d1ceda4f1862f42c
html-css-js23 <129732023+html-css-js23@users.noreply.github.com>
2023-12-10 11:55:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,44 @@
-// Select elements
-const purchaseButton = document.querySelector('.purchase');
-const cartButton = document.querySelector('.cart button');
-const cart Ul = document.querySelector('.cart ul');
-const quantityInputs = document.querySelectorAll('.cart li input');
-const quantityButtons = document.querySelectorAll('.cart li button');
-
-// Add event listener to purchase button
+// Select the purchase button and add an event listener
+const purchaseButton = document.querySelector('.purchase-button');
 purchaseButton.addEventListener('click', () => {
-    // Toggle cart display
-    const cartSection = document.querySelector('.cart');
-    cartSection.classList.toggle('show');
-
-    // Add item to cart
-    const itemName = 'Test Item1';
-    const itemQuantity = 1;
-    const cartItem = { name: itemName, quantity: itemQuantity };
-    cartUl.innerHTML += `<li>${itemName} x ${itemQuantity}</li>`;
-
-    // Update cart quantity inputs
-    quantityInputs.forEach((input, index) => {
-        input.value = itemQuantity;
-    });
+    // Toggle the display of the cart and checkout forms
+    const cartForm = document.querySelector('.cart form');
+    const checkoutForm = document.querySelector('.checkout form');
+    cartForm.classList.toggle('hidden');
+    checkoutForm.classList.toggle('hidden');
 });
 
-// Add event listener to cart button
-cartButton.addEventListener('click', () => {
-    // Toggle cart display
-    const cartSection = document.querySelector('.cart');
-    cartSection.classList.toggle('show');
-});
-
-// Add event listener to quantity inputs
-quantityInputs.forEach((input) => {
-    input.addEventListener('input', () => {
-        // Update cart quantity
-        const quantity = parseInt(input.value);
-        const itemName = input.name;
-        const cartItem = { name: itemName, quantity };
-        cartUl.innerHTML = `<li>${itemName} x ${quantity}</li>`;
-    });
-});
-
-// Add event listener to quantity buttons
-quantityButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-        // Update cart quantity
-        const quantity = parseInt(button.previousSibling.value);
-        const itemName = button.previousSibling.name;
-        const cartItem = { name: itemName, quantity };
-        cartUl.innerHTML = `<li>${itemName} x ${quantity}</li>`;
+// Select the checkout form and add an event listener
+const checkoutForm = document.querySelector('.checkout form');
+checkoutForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // Get the quantity and name from the form
+    const quantity = document.querySelector('#quantity').value;
+    const name = document.querySelector('#name').value;
+    // Get the delivery or takeout option
+    const delivery = document.querySelector('#delivery').checked;
+    // Calculate the total cost
+    const totalCost = quantity * 10; // Replace this with the actual cost of the item
+    // Send the order details to the server using fetch
+    fetch('https://example.com/api/orders', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name,
+            quantity,
+            delivery,
+            totalCost
+        })
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        // Display a success message
+        alert('Order placed successfully!');
+    })
+    .catch((error) => {
+        // Display an error message
+        alert('Error placing order: ' + error);
     });
 });
